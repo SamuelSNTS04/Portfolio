739f0f0b19ff7e49ccae99483cf5f42f9f8cfead
SamuelSNTS04 <malakaigamer5@gmail.com>
2025-09-08 20:14:20 -0300

    Finalizando a parte Portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,10 +48,22 @@ function updateLanguages(profileData) {
     languages.innerHTML = profileData.languages.map(language => `<li>${language}</li>`).join('');
 }
 
+function updatePortfolio(profileData) {
+    const portfolio = document.getElementById('profile.portfolio');
+
+    portfolio.innerHTML = profileData.portfolio.map(project => 
+          `<li>
+            <h3 ${project.github ? 'class="github"' : ''}>${project.name}</h3>
+            <a href="${project.url}" target="_blank">${project.url}</a>
+          </li>`)
+    .join('');
+}
+
 (async () => {
     const profileData = await fetchProfileData();
     updateProfileData(profileData);
     updateSoftSkills(profileData);
     updateHardSkills(profileData);
     updateLanguages(profileData);
+    updatePortfolio(profileData);
 })()
